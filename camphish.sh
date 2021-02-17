7f3a641a8d8ddf0a45bc56163b4c2d1b010eaa79
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-02-17 10:46:44 +0600

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -1,3 +1,41 @@
+#! /data/data/com.termux/files/usr/bin/bash
+clear
+echo "                   ░██████╗██████╗░███╗░░░███╗██╗░░██╗"
+echo "                   ██╔════╝██╔══██╗████╗░████║██║░░██║"
+echo "                   ╚█████╗░██║░░██║██╔████╔██║███████║"
+echo "                   ░╚═══██╗██║░░██║██║╚██╔╝██║██╔══██║"
+echo "                   ██████╔╝██████╔╝██║░╚═╝░██║██║░░██║"
+echo "                   ╚═════╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝"
+echo "          _____________________________________________________"
+echo "          ██╗░░██╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗░██████╗░"
+echo "          ██║░░██║██╔══██╗██╔══██╗██║░██╔╝██║████╗░██║██╔════╝░"
+echo "          ███████║███████║██║░░╚═╝█████═╝░██║██╔██╗██║██║░░██╗░"
+echo "          ██╔══██║██╔══██║██║░░██╗██╔═██╗░██║██║╚████║██║░░╚██╗"
+echo "          ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝"
+echo "         ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░"
+echo "                          ████████╗██╗░░░██╗"
+echo "                          ╚══██╔══╝██║░░░██║"
+echo "                          ░░░██║░░░╚██╗░██╔╝"
+echo "                          ░░░██║░░░░╚████╔╝░"
+echo "                          ░░░██║░░░░░╚██╔╝░░"
+echo "                          ░░░╚═╝░░░░░░╚═╝░░░"
+echo "                               WELCOME"
+echo "                SDMH-Hacking-TV       Just Email Bombing"
+
+echo                                                        
+  read -p "Use this tool for educational purpose only press enter to continue " Enter
+echo
+clear
+
+#!/bin/bash
+#installation
+clear
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
+xdg-open https://syr.us/iiW
+echo ""
+
 #!/bin/bash
 # Xophisher v1.0
 # Powered by Xowmik Xoss
